8cf9e3c6c8d95b7b43c2a963ba0538ef58abf608
khalid <kalidkalil4254@gmail.com>
2022-08-28 16:46:33 +0200

    final-1

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { deleteLocalFiles, files ,refactoredMethod} from './util/util';
-
+import { deleteLocalFiles, files ,filterImageFromURL} from './util/util';
 
 (async () => {
-
   // Init the Express application
   const app = express();
-
   // Set the network port
   const port = process.env.PORT || 8082;
-  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}} <== ( as query).
@@ -30,28 +24,20 @@ import { deleteLocalFiles, files ,refactoredMethod} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
-
   app.get( "/filteredimage", async ( req, res ,next) => {
     let {image_url}= req.query;
-  
      //    1. validate the image_url query
     if(!image_url){
       res.status(403).send("Imge URL is Required!!!");
     }
      //    2. call filterImageFromURL(image_url) to filter the image
-    await refactoredMethod(image_url as unknown as string);
-    console.log("saved files locally!");
+    await filterImageFromURL(image_url as unknown as string);
      //    3. send the resulting file in the response
-     console.log(files[files.length-1]);
-   setTimeout(()=>{res.status(200).sendFile(files[files.length-1]);},1000);
-   console.log("sent files as response!");
-    
+   setTimeout(()=>{res.status(200).sendFile(files[files.length-1]);},1000);  
     next();
   } ,(req,res,next)=>{
      //    4. deletes any files on the server on finish of the response
      setTimeout(()=>{  deleteLocalFiles(files);},12000);
-     console.log("deleted files!");
   });
 
   //! END @TODO1
--- src/util/util.ts
+++ src/util/util.ts
@@ -4,6 +4,11 @@ import axios from 'axios';
 import { resolve } from "path";
 import { TIMEOUT } from "dns";
 export let files:string[]=[];
+
+
+
+
+
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -11,37 +16,11 @@ export let files:string[]=[];
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-
-//a method to filter a given image by url and returns a promise 
-
-
-// export async function filterImageFromURL(inputURL: string): Promise<string> {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       console.log("start reading image from src...");
-//           //converting the image into buffer first: 
-
-//           refactoredMethod(inputURL);
-
-//       const photo= await Jimp.read(inputURL)
-//       console.log(inputURL);
-//       const outpath: string = `/tmp/filtered_${Math.floor(Math.random()*2000)}.jpg`; //setting a new name for the image inside the directory to store it in
-//       files.push(`${__dirname}${outpath}`);      
-//         // photo
-//         // .resize(256, 256) //1- resize
-//         // .quality(60) //2- set JPEG quality
-//         // .greyscale() //3- set greyscale
-//         // .write(__dirname + outpath, (img) => {
-//         //   resolve(__dirname + outpath); // 4- write the filtered image into a directory
-//         // });      
-//     } catch (error) {
-//       reject(error);
-//     }
-//   });
-// }
-
-  //returns the buffer promise of specified image======================================
-export async function refactoredMethod(imgUrl:string){
+//using axios to handle getting image request instead of Jimp handling due to error
+//it requests the image as a client as i understood <===
+//occured when reading it ==> Error: MIME not found for buffer
+//returns the buffer promise of specified image======================================
+export async function filterImageFromURL(imgUrl:string){
   const outpath: string = `/tmp/filtered_${Math.floor(Math.random()*2000)}.jpg`; //setting a new name for the image inside the directory to store it in
   files.push(`${__dirname}${outpath}`);
   await axios({
@@ -66,27 +45,8 @@ export async function refactoredMethod(imgUrl:string){
 }
 
 
-
-// async function getImg(imgUrl:string) {
-//   try {
-//     const response = await axios.get(imgUrl);
-//     let img=response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-//   getImg("https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg");
-
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
-
 //deleting the files in the filtered images directory
 export function deleteLocalFiles(files: Array<string>) {
-  console.log("root dir:"+__dirname);
   for (let file of files) {
     fs.unlinkSync(file);
     files.shift();
